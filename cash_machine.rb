95f64177f087ff7611a072eeb3a74c510c8754fb
Marcin Drozd <marcindrozd@me.com>
2016-01-24 12:09:31 +0100

    add remaining cash machine operations to logger

--- cash_machine.rb
+++ cash_machine.rb
@@ -20,15 +20,18 @@ class CashMachine
     end
 
     inserted_card = Card.new('1234')
-    ATMLogger.log.info "Inserted card number ending #{inserted_card.number[12..-1]}"
-
+    card_number = inserted_card.number[12..-1]
+    
     pin = get_pin
 
     if inserted_card.pin_correct?(pin) && !inserted_card.disabled?
+      ATMLogger.log.info "Inserted card number ending in #{card_number}"
       handle_correct_card_and_pin(inserted_card)
     elsif inserted_card.disabled?
+      ATMLogger.log.info "Inserted disabled card ending in #{card_number}"
       puts 'This card has been disabled. Please contact your bank to enable it.'
     else
+      ATMLogger.log.info "PIN for card ending in #{card_number} was incorrect. Card was disabled."
       puts 'The PIN number you entered is incorrect. The card has been disabled.'
       inserted_card.disable
     end
@@ -52,6 +55,7 @@ class CashMachine
 
   def handle_correct_card_and_pin(inserted_card)
     selected_option = ''
+    card_number = inserted_card.number[12..-1]
 
     loop do
       display_menu
@@ -66,8 +70,8 @@ class CashMachine
 
     case selected_option
     when '1'
-      puts "\nYour Balance: #{inserted_card.get_balance}"
-      puts
+      ATMLogger.log.info "Successfully checked balance for card ending in #{card_number}."
+      puts "\nYour Balance: #{inserted_card.get_balance}\n"
       another_operation?(inserted_card)
     when '2'
       puts 'How much money would you like to withdraw?'
@@ -75,12 +79,15 @@ class CashMachine
       amount = gets.chomp.to_i
 
       if amount > inserted_card.get_balance
+        ATMLogger.log.info "Attempted to retrieve more money than available on card #{card_number}."
         puts "Sorry. You don't have enough money on your account to withdraw #{amount} PLN"
         another_operation?(inserted_card)
       elsif amount > available_cash
+        ATMLogger.log.info "Attempted to retrieve more money than available in cash machine #{card_number}."
         puts 'Sorry. There is currently not enough cash available in this ATM Machine to withdraw such amount.'
         another_operation?(inserted_card)
       else
+        ATMLogger.log.info "Successfully withdrawn money for card #{card_number}."        
         puts "Withdrawing #{amount} PLN"
         inserted_card.withdraw(amount)
         another_operation?(inserted_card)
@@ -90,11 +97,13 @@ class CashMachine
 
       amount = gets.chomp.to_i
 
+      ATMLogger.log.info "Successfully deposited money for card ending in #{card_number}."
       puts "Depositing #{amount} PLN"
       inserted_card.deposit(amount)
 
       another_operation?(inserted_card)
     when '0'
+      ATMLogger.log.info "Exit successful."
       puts 'Exiting'
     end
   end
